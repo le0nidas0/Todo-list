fcdaea36a115103d78c6859f7e9f5b1d36e3859b
leonidas <leonidasmoreira098@gmail.com>
2025-02-22 19:06:35 -0300

    feat: implement Task service with CRUD operations

---
+++ todo-list-api/src/main/java/com/example/todo_list_api/service/TaskService.java
@@ -0,0 +1,46 @@
+package com.example.todo_list_api.service;
+
+import com.example.todo_list_api.repository.TaskRepository;
+import com.example.todo_list_api.task.Task;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TaskService {
+    private final TaskRepository taskRepository;
+
+    public TaskService(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+
+    public Task createTask(Task task) {
+        return taskRepository.save(task);
+    }
+
+    public List<Task> getAllTasks() {
+        return taskRepository.findAll();
+    }
+
+    public Task getTaskById(Long id) {
+        return taskRepository.findById(id).orElseThrow(() -> new RuntimeException("Task not found"));
+    }
+
+    public Task updateTask(Long id, Task updatedTask) {
+        Task existingTask = getTaskById(id);
+        existingTask.setTitle(updatedTask.getTitle());
+        existingTask.setDescription(updatedTask.getDescription());
+        existingTask.setStatus(updatedTask.getStatus());
+        return taskRepository.save(existingTask);
+    }
+
+    public boolean deleteTask(Long id) {
+        if (taskRepository.existsById(id)) {
+            taskRepository.deleteById(id);
+            return true;
+        }
+        return false;
+    }
+
+
+}
