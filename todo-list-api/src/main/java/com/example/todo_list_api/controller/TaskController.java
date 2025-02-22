dcd0e290714c838ffa08317d7531388f47de3864
leonidas <leonidasmoreira098@gmail.com>
2025-02-22 19:08:54 -0300

    feat: create Task controller with REST endpoints

---
+++ todo-list-api/src/main/java/com/example/todo_list_api/controller/TaskController.java
@@ -0,0 +1,63 @@
+package com.example.todo_list_api.controller;
+
+import com.example.todo_list_api.service.TaskService;
+import com.example.todo_list_api.task.Task;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/task")
+public class TaskController {
+
+    @Autowired
+    private TaskService taskService;
+
+    @PostMapping
+    public ResponseEntity<Task> creatTask(@RequestBody Task task) {
+        Task createdTask = taskService.createTask(task);
+        return new ResponseEntity<>(createdTask, HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Task>> getAlltasks() {
+        List<Task> tasks = taskService.getAllTasks();
+        return new ResponseEntity<>(tasks, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Task> getTaskById(@PathVariable Long id) {
+        Task task = taskService.getTaskById(id);
+        if (task != null) {
+            return new ResponseEntity<>(task, HttpStatus.OK);
+        }else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Task> updateTask(@PathVariable Long id, @RequestBody Task taskDetails) {
+        Task updatedTask = taskService.updateTask(id, taskDetails);
+        if (updatedTask != null) {
+            return new ResponseEntity<>(updatedTask, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteTask(@PathVariable Long id) {
+        boolean isDeleted = taskService.deleteTask(id);
+        if (taskService.deleteTask(id)) {
+            return new ResponseEntity<>("Task deleted successfully", HttpStatus.NO_CONTENT);
+        } else {
+            return new ResponseEntity<>("Task not found", HttpStatus.NOT_FOUND);
+        }
+    }
+
+
+
+}
